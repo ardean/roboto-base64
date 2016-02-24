f1f64e38cf335ad42903b8c50be5e8d25016e3eb
ardean <aron.stifani@gmail.com>
2016-02-24 13:14:00 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "Roboto-Regular.ttf": require("roboto-fontface/fonts/Roboto-Regular.ttf"),
+  "Roboto-Medium.ttf": require("roboto-fontface/fonts/Roboto-Medium.ttf"),
+  "Roboto-Italic.ttf": require("roboto-fontface/fonts/Roboto-RegularItalic.ttf")
+};
